e6f84ed1e88be9c330a4a18de797e9472f07cbde
root <root@populis.internal>
2021-07-05 09:41:04 +0000

    Api views working

--- routes/news.js
+++ routes/news.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Deputy = require("../models/Deputy.model");
 const axios = require("axios");
-const deputiesAPI = "https://raw.githubusercontent.com/centraldedados/parlamento-deputados/master/data/deputados.json"
 const parliamentAPI = "https://app.parlamento.pt/webutils/docs/doc.txt?path=6148523063446f764c324679626d56304c3239775a57356b595852684c3052685a47397a51574a6c636e52766379394a626d5a76636d3168773666446f32386c4d6a424359584e6c4c31684a566955794d45786c5a326c7a6247463064584a684c306c755a6d39796257466a5957394359584e6c57456c575832707a6232347564486830&fich=InformacaoBaseXIV_json.txt"
 
 //
 router.get("/news", async(req, res) => {
     const response = await axios.get(parliamentAPI);
     const news = response.data;
-    console.log(news);
+    const gruposParl = news.Legislatura.GruposParlamentares.pt_gov_ar_objectos_GPOut
+    console.log(gruposParl);
+    // console.log(news);
     res.render("news", { layout: false, news });
 });
 
@@ -27,12 +28,12 @@ router.get("/news", async(req, res) => {
 //not working
 router.get("/news/:newsId", async(req, res, next) => {
     try {
-        const findnewsId = findById(req.params.newsId)
+        const findnewsId = req.params.newsId;
         const newsDetail = await axios.get(parliamentAPI)
-        const newsById = newsDetail.data.id;
-        // console.log(id)
-        // // console.log(newsDetail)
-        // console.log(newsById)
+        const newsById = newsDetail.data;
+        console.log(newsDetail.data)
+            // // console.log(newsDetail)
+            // console.log(newsById)
         res.render("news-detail", { layout: false, newsDetail });
     } catch (err) {
         next(err);
