8ef274cacc9f5f49a09a4cbf6383113128dd8e22
root <root@populis.internal>
2021-07-07 12:37:46 +0000

    normal

--- routes/voting.js
+++ routes/voting.js
@@ -11,14 +11,11 @@ router.get("/voting", async(req, res, next) => {
         const response = await axios.get(parliamentAPI);
         const voting = response.data;
         console.log(voting)
+        const party = await Party.find().sort({ nome: 1 })
         res.render("parliament/voting", { voting, party });
     } catch (err) {
         next(err);
     }
-});
-
-router.get("/voting", (req, res) => {
-    res.render("parliament/voting", { Party });
 });
 
 module.exports = router;
