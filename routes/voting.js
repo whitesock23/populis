363ad9c1e33c8c34ed75cabb93419fcc7cbcdf3c
root <root@populis.internal>
2021-07-08 20:09:34 +0000

    another push

--- routes/voting.js
+++ routes/voting.js
@@ -18,21 +18,21 @@ const parliamentAPI = "https://app.parlamento.pt/webutils/docs/doc.txt?path=6148
  */
 function requireLogin(req, res, next) {
     if (req.session.currentUser) {
-      next();
-    } else {
-      res.redirect("/login");
-    }
-  }
-  
-  function requireAdmin(req, res, next){
-    if (req.session.currentUser &&
-      req.session.currentUser.role === "Admin"){
         next();
-      } else {
+    } else {
+        res.redirect("/login");
+    }
+}
+
+function requireAdmin(req, res, next) {
+    if (req.session.currentUser &&
+        req.session.currentUser.role === "Admin") {
+        next();
+    } else {
         res.redirect('/login');
-      }
-  }
-  
+    }
+}
+
 
 router.get("/voting", async(req, res, next) => {
     try {
@@ -46,48 +46,18 @@ router.get("/voting", async(req, res, next) => {
     }
 });
 
-router.get('/polls',  (req, res, next) => {
+router.get('/polls', (req, res, next) => {
     Poll.find().exec((err, polls) => {
         res.render('parliament/polls', { polls: polls });
     });
 });
 
-// router.post('/:pollId/vote', (req, res, next) => {
-//     const choice = req.body.choice;
-//     const identifier = `choices.${choice}.votes`;
-//     Poll.update({ _id: req.params.pollId }, {
-//         $inc: {
-//             [identifier]: 1
-//         }
-//     }, {}, (err, numberAffected) => {
-//         res.send('');
-//     });
-// });
-
 
 router.post('/:pollId/vote', (req, res, next) => {
     const choice = req.body.choice;
-    const UserVotes = `choices.${choice}.votes`;
-    console.log('sushi:', UserVotes);
-    if (UserVotes === 'votes'){
-        res.render('/menu', UserVotes);
-    } 
-    Poll.update({ _id: req.params.pollId }, {
-        $inc: {
-            [UserVotes]: 1
-        }
-    }, {}, (err, numberAffected) => {
-        let Pusher = require('pusher');
-        let pusher = new Pusher({
-            appId: process.env.PUSHER_APP_ID,
-            key: process.env.PUSHER_APP_KEY,
-            secret: process.env.PUSHER_APP_SECRET,
-            cluster: process.env.PUSHER_APP_CLUSTER
-        });
-
-        let payload = { pollId: req.params.pollId, choice: choice };
-        pusher.trigger('poll-events', 'vote', payload, req.body.socketId);
-
+    const identifier = `choices.${choice}.votes`;
+    Poll.update({ _id: req.params.pollId }, { $inc: {
+            [identifier]: 1 } }, {}, (err, numberAffected) => {
         res.send('');
     });
 
