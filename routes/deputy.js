ac498ca7db52056c58ee5e49112a7ff85f96ce96
root <root@populis.internal>
2021-07-09 14:02:06 +0000

    final main

--- routes/deputy.js
+++ routes/deputy.js
@@ -1,16 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const Deputy = require("../models/Deputy.model");
+const Party = require("../models/Party.model");
 const axios = require("axios");
 const parliamentAPI = "https://app.parlamento.pt/webutils/docs/doc.txt?path=6148523063446f764c324679626d56304c3239775a57356b595852684c3052685a47397a51574a6c636e52766379394a626d5a76636d3168773666446f32386c4d6a424359584e6c4c31684a566955794d45786c5a326c7a6247463064584a684c306c755a6d39796257466a5957394359584e6c57456c575832707a6232347564486830&fich=InformacaoBaseXIV_json.txt"
 
-router.get("/deputies", async(req, res) => {
-    const response = await axios.get(parliamentAPI);
-    const deputies = response.data;
-    const deputiesArray = deputies.Legislatura.Deputados.pt_ar_wsgode_objectos_DadosDeputadoSearch;
-    console.log(deputiesArray);
+// router.get("/deputies", async(req, res) => {
+//     const response = await axios.get(parliamentAPI);
+//     const deputies = response.data;
+//     const deputiesArray = deputies.Legislatura.Deputados.pt_ar_wsgode_objectos_DadosDeputadoSearch;
+//     console.log(deputiesArray);
+//     res.render("deputies/deputies-list", { deputies });
+// });
+
+router.get("/deputies-locations", async(req, res) => {
+    const deputies = await Deputy.find();
+    const circulosArr = []
+
+    deputies.forEach((e) => {
+        circulosArr.push(e.circulo);
+    });
+
+    // console.log(circulosArr);
+
+    let uniqueCirculos = [...new Set(circulosArr)];
+    // console.log(uniqueCirculos);
+    res.render('deputies/circulos', { uniqueCirculos });
+})
+
+router.get("/deputies/:circulo", async(req, res) => {
+    const theCirculo = req.params.circulo;
+    // console.log(theCirculo);
+    const deputies = await Deputy.find({ circulo: theCirculo })
+    console.log(deputies);
     res.render("deputies/deputies-list", { deputies });
-});
+})
+
+router.get("/parties", async(req, res) => {
+    const parties = await Party.find()
+    res.render("parliament/parties", { parties });
+})
+
 
 router.get("/deputies/:deputyId", async(req, res) => {
     const deputyDetail = await deputyAPI.findById(req.params.deputyId);
